39cbcc6787063211eef6d0eaf56df9a695cfcecf
Valeriia Melnikov <valery.medvedeva@gmail.com>
2022-10-28 18:40:44 +0300

    feat: add score field

--- script.js
+++ script.js
@@ -2,8 +2,13 @@
 
 let guessNumber = Number(Math.trunc(Math.random() * 20) + 1);
 let score = 20;
+let highScore = 0;
 
-document.querySelector('.again').addEventListener('click', function(){
+const showMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+}
+
+document.querySelector('.again').addEventListener('click', function () {
     guessNumber = Number(Math.trunc(Math.random() * 20) + 1);
     score = 20;
     document.querySelector('body').style.backgroundColor = '#222';
@@ -11,32 +16,31 @@ document.querySelector('.again').addEventListener('click', function(){
     document.querySelector('.number').textContent = '?';
     document.querySelector('.guess').value = '';
     document.querySelector('.score').textContent = score;
-    document.querySelector('.message').textContent = "Start guessing...";
-    
+    showMessage("Start guessing...");
+
 });
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
-
     if (!guess) {
-        document.querySelector('.message').textContent = "No number!"
+        showMessage("No number!");
     } else if (guess === guessNumber) {
-        document.querySelector('.message').textContent = "Correct number!"
+        showMessage("Correct number!");
         document.querySelector('.number').textContent = guessNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
+        if (score > highScore) {
+            console.log(score);
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
     } else if (score > 1) {
-        if (guess > guessNumber) {
-            document.querySelector('.message').textContent = "Too high.."
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else if (guess < guessNumber) {
-            document.querySelector('.message').textContent = "Too low.."
+        if (guess !== guessNumber) {
+            guess > guessNumber ? showMessage("Too high..") : showMessage("Too low..");
             score--;
             document.querySelector('.score').textContent = score;
         }
     } else {
         document.querySelector('.score').textContent = 0;
-        document.querySelector('.message').textContent = "You lost the game =("
+        showMessage("You lost the game =(");
     }
 });
